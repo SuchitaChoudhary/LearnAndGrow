057473255629618b1cb9dea664bb09c5332666c2
SuchitaChoudhary <83327632+SuchitaChoudhary@users.noreply.github.com>
2021-04-28 10:13:56 +0530

    Created Student class
    it is having main method 
    one variable

---
+++ Student.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class A
+{
+}
+public class Student
+{
+public static void main(String args[])
+{
+Scannner sc=new Scanner(System.in);
+int rollnumber=sc.nextInt();
+A a=new A();
+a.readRollNo(rollNumber)
+}
+}
